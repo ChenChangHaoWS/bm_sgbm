e1a7682e732f68ed5eeb1a08fe2eb6073c74a2a5
cch <37094807+ChenChangHaoWS@users.noreply.github.com>
2019-02-26 16:24:53 +0800

    Update bm.py

--- src/bm.py
+++ src/bm.py
@@ -2,14 +2,12 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-print('loading images...')
 imgL = cv2.imread('../data/left.png', 0)
 imgR = cv2.imread('../data/right.png', 0)
 
 # SAD window size should be between 5..255
 block_size = 15
 
-min_disp = 0
 num_disp = 16    # 必须取16的整数倍
 uniquenessRatio = 10
 
@@ -21,10 +19,14 @@ stereo.setUniquenessRatio(uniquenessRatio)
 # disparity = stereo.compute(imgL,imgR)
 disparity = stereo.compute(imgL, imgR).astype(np.float32) / 16.0  
 
-print "saving disparity as disparity_image_bm.txt"
-np.savetxt("../data/disparity_image_bm.txt", disparity, fmt='%3.2f', delimiter=' ', newline='\n')
+
+# np.savetxt("../data/disparity_image_bm.txt", disparity, fmt='%3.2f', delimiter=' ', newline='\n')
 
 
-print "plotting disparity"
 plt.imshow(disparity,'gray')
+plt.axis('off')   # 去掉坐标轴
+plt.savefig('...', disparity)
 plt.show()
+
+disparity = cv2.imread('', cv2.IMREAD_GRAYSCALE)
+disparity = cv2.applyColorMap(disparity, cv2.COLORMAP_JET)    # 转化为伪彩色图
